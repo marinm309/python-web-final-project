429e8ff452eca44b5becc367e453cd6d90b81185
marinm309 <marinm309@abv.bg>
2022-11-13 16:44:02 +0200

    done with user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import AppUser
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+UserModel = get_user_model()
+
+@admin.register(UserModel)
+class AppUserAdmin(UserAdmin):
+    ordering = ('email', )
+    list_display = ['email', 'date_joined', 'last_login', ]
+    list_filter = ()
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        #(('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (('Permissions'), {
+            'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (('Important dates'), {'fields': ('last_login', )}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from .managers import AppUserManager
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    date_join = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
     is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
