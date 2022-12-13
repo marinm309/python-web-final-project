d1017a4389fcfa4147cfffae961f6e9ba8a8af20
marinm309 <marinm309@abv.bg>
2022-12-13 18:59:34 +0200

    asdf

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('register/', views.UserCreationView.as_view(), name='register'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile', views.edit_profile, name='edit-profile'),
+    path('delete_profile', views.delete_profile, name='delete-profile'),
+    path('delete_profile_final', views.delete_profile_final, name='delete-profile-final'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from products.models import ShippingAddress, Order, Products
 
 
 UserModel = get_user_model()
@@ -26,3 +28,40 @@ class UserCreationView(views.CreateView):
     
 class UserLogoutView(LogoutView):
     pass
+
+
+def profile(request):
+    user = request.user
+    customer = user.customer
+    address = ShippingAddress.objects.filter(customer=customer)
+    orders = [x.order for x in address] if address else ''
+    order_items = [x.orderitem_set.all() for x in orders] if orders else ''
+    products = [x.product for x in order_items[0]] if order_items else ''
+    context = {
+        'customer': customer,
+        'products': products
+    }
+    return render(request, 'users/profile.html', context)
+
+def edit_profile(request):
+    pass
+
+def delete_profile(request):
+    user = request.user
+    customer = user.customer
+    address = ShippingAddress.objects.filter(customer=customer)
+    orders = [x.order for x in address] if address else ''
+    order_items = [x.orderitem_set.all() for x in orders] if orders else ''
+    products = [x.product for x in order_items[0]] if order_items else ''
+    context = {
+        'customer': customer,
+        'products': products
+    }
+    return render(request, 'users/delete_profile.html', context)
+
+def delete_profile_final(request):
+    user = request.user
+    customer = user.customer
+    customer.delete()
+    user.delete()
+    return redirect('logout')
