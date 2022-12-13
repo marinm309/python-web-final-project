1af3e05b5438cb591464e1e08aa43a1560f70159
marinm309 <marinm309@abv.bg>
2022-12-14 00:37:46 +0200

    done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document=settings.STATIC_ROOT)
