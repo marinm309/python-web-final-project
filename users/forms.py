d29eb6f53faf074a9f47ba2231478cac7c1d8925
marinm309 <marinm309@abv.bg>
2022-12-18 00:07:36 +0200

    done

--- users/forms.py
+++ users/forms.py
@@ -16,7 +16,7 @@ class CustomAuthenticationForm(AuthenticationForm):
 
     def __init__(self, request=None, *args, **kwargs):
         super().__init__(request=None, *args, **kwargs)
-        self.fields['username'].label = 'Username'
+        self.fields['username'].label = 'Email'
         self.fields['password'].label = 'Password'
 
 class SignUpForm(UserCreationForm):
