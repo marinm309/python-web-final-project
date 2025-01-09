5f76dd5eec97f23e9688423289c34bc40946cd2d
marinm309 <93922242+marinm309@users.noreply.github.com>
2025-01-09 19:50:53 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'bbRITv4Wo5CMyocFGcPE',
-#         'HOST': 'containers-us-west-126.railway.app',
-#         'PORT': '7200',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bbRITv4Wo5CMyocFGcPE',
+        'HOST': 'containers-us-west-126.railway.app',
+        'PORT': '7200',
+    }
+}
 
 
 # Password validation
